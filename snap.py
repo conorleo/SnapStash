c560d3cce2759018f5e33111ea19a1eb41c204c7
conorleo <conorleo123@gmail.com>
2026-04-29 21:24:15 +0100

    feat: callback to increment iRank on scroll

--- snap.py
+++ snap.py
@@ -2,18 +2,37 @@ import keyboard
 import mouse
 import time
 
+iRank = 0 # index of the current selected window out of the set of windows currently containing the cursor (0 => smallest window containing the cursor will be selected)
+
+def on_scroll(event):
+    """
+    Callback function triggered when the mouse is scrolled.
+    event.delta > 0 means scroll up, event.delta < 0 means scroll down.
+    Scroll up increments iRank by 1.
+    Scroll down decrements iRank by 1.
+    """
+    global iRank
+    if event.delta > 0:  # Scroll up
+        iRank += 1
+        print(f"iRank incremented: {iRank}")
+    elif event.delta < 0:  # Scroll down
+        iRank -= 1
+        print(f"iRank decremented: {iRank}")
+
 # getScreen() # capture current screen
 
 # getWindows() # output list of windows identified in current screen
 
 # arrange windows in order of increasing area
 
-iRank = 0 # index of the current selected window out of the set of windows currently containing the cursor (0 => smallest window containing the cursor will be selected)
+# Hook scroll events
+mouse.hook(on_scroll)
 
 while True:
     # Exit program if ESC key pressed
     if keyboard.is_pressed("esc"):
         print("\nESC key detected. Exiting...")
+        mouse.unhook_all()
         break
 
     # Save snap if left-click is detected
@@ -22,11 +41,8 @@ while True:
         # crop() # crop img to region defined by current window
         # tag() # tag the img
         # save() # save the img
+        mouse.unhook_all()
         break
-
-    # Increment iRank if scroll up detected
-
-    # Decrement iRank if scroll down detected
 
     # Temporarily add delay
     time.sleep(0.05)
